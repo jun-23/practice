b533a16c575fc8dc96a9057a303d11974c874b60
jun-23 <jujng9699@gmail.com>
2023-11-07 15:51:39 +0900

    backjoon #24262

---
+++ Main.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Main {
+
+    public static int MenOfPassion(int a[], int n) {
+        int i = n / 2;
+        return a[i];
+    }
+
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        int num = scanner.nextInt();
+
+        System.out.println(1);
+        System.out.println(0);
+    }
+
+}
